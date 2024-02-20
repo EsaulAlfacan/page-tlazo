f5de9b6b56688b6ec4f562f264c39bd9b3b25c8a
EsaulAlfacan <esaulalfacan@gmail.com>
2024-02-20 15:09:01 -0600

    PRECIO

--- JS/articulo-seleccionado.js
+++ JS/articulo-seleccionado.js
@@ -2,28 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const articulos = document.querySelectorAll(".platillo");
 
   articulos.forEach((articulo) => {
-    // Se añade el botón de cierre a cada artículo desde el principio para evitar recrearlo en cada clic
     const botonCierre = document.createElement("button");
-    botonCierre.textContent = "×"; // Carácter de tache
-    botonCierre.classList.add("boton-cierre", "oculto"); // Añade una clase 'oculto' para esconderlo inicialmente
+    botonCierre.textContent = "×";
+    botonCierre.classList.add("boton-cierre", "oculto");
 
-    // Función para abrir el artículo en pantalla completa
     const abrirArticulo = () => {
-      // Remueve la clase 'oculto' para mostrar el botón de cierre
       botonCierre.classList.remove("oculto");
       articulo.classList.add("articulo-en-pantalla-completa");
       articulo.appendChild(botonCierre);
     };
 
-    // Función para cerrar el artículo y ocultar el botón de cierre
     const cerrarArticulo = () => {
       articulo.classList.remove("articulo-en-pantalla-completa");
-      botonCierre.classList.add("oculto"); // Añade la clase 'oculto' para esconder el botón
+      botonCierre.remove(); // Elimina el botón de cierre del DOM
     };
 
-    // Evento para abrir el artículo en pantalla completa
     articulo.addEventListener("click", function (e) {
-      // Asegura que el clic no sea en el botón de cierre ni en un artículo ya abierto
       if (
         !this.classList.contains("articulo-en-pantalla-completa") &&
         e.target !== botonCierre
@@ -32,10 +26,27 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    // Añade el evento al botón de cierre para cerrar el artículo
     botonCierre.addEventListener("click", function (e) {
       cerrarArticulo();
-      e.stopPropagation(); // Previene que el evento se propague al artículo
+      e.stopPropagation(); // Previene la propagación para evitar que el evento de clic se propague al artículo
     });
+  });
+
+  // Selecciona el botón de añadir utilizando su ID
+  const botonAnadirElemento = document.getElementById("botonAnadirElemento");
+
+  // Añade el evento al botón de añadir para cerrar el artículo abierto
+  botonAnadirElemento.addEventListener("click", function () {
+    // Encuentra y cierra el artículo que está actualmente en pantalla completa
+    const articuloAbierto = document.querySelector(
+      ".articulo-en-pantalla-completa"
+    );
+    if (articuloAbierto) {
+      articuloAbierto.classList.remove("articulo-en-pantalla-completa");
+      const botonCierre = articuloAbierto.querySelector(".boton-cierre");
+      if (botonCierre) {
+        botonCierre.remove(); // Asegura que el botón de cierre también se elimine
+      }
+    }
   });
 });
